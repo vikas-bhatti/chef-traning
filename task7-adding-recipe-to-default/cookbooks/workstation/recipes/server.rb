386a4c2deddb25703071cdc2d6ed2c4990e7f8b2
vikas-bhatti <vikas.bhatti@ymail.com>
2021-05-14 06:52:34 +0100

    task7

---
+++ task7-adding-recipe-to-default/cookbooks/workstation/recipes/server.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook:: workstation
+# Recipe:: server.rb
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+package 'httpd' do
+    action :remove
+end
+
+package 'httpd' do
+    action :install
+end
+
+service 'httpd' do 
+    action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+    content 'webserver to create new recipe for task7'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
--- task7-adding-recipe-to-default/cookbooks/workstation/recipes/server.rb.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook:: workstation
-# Recipe:: server.rb
-#
-# Copyright:: 2021, The Authors, All Rights Reserved.
